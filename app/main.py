6982ef090808853b4f9c9ec7969e1afa819142df
Ivan Synytskyi <ivansinickij29@icloud.com>
2026-03-06 19:58:52 +0100

    fix

--- app/main.py
+++ app/main.py
@@ -5,12 +5,14 @@ class Car:
         self.brand = brand
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int, average_rating: int, count_of_rating: int) ->None:
+    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int,
+                 count_income: int
+                 ) ->None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
-        self.count_of_rating = count_of_rating
-        self.total_income = 0
+        self.count_of_ratings = count_of_ratings
+        self.total_income = count_income
 
 
     def calculate_washing_price(self, car: Car) -> float:
@@ -19,25 +21,23 @@ class CarWashStation:
         price = car.comfort_class * (
                 self.clean_power - car.clean_mark
         ) * self.average_rating / self.distance_from_city_center
-        return price
+        return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark > self.clean_power:
             car.clean_mark = self.clean_power
 
-    def serve_car(self, car: Car) -> float:
-        if car.clean_mark > self.clean_power:
-            price = car.comfort_class * (
-                        self.clean_power - car.clean_mark
-            ) * self.average_rating / self.distance_from_city_center
+    def serve_car(self, cars: Car) -> float:
+        if self.clean_power > cars.clean_mark:
+            price = self.calculate_washing_price(cars)
             self.total_income += price
-            self.wash_single_car(car)
+            self.wash_single_car(cars)
             return price
         return 0
 
     def rate_service(self, rate: float) -> float:
-        new_price = (self.average_rating * self.count_of_rating + rate) / (self.count_of_rating + 1)
-        self.count_of_rating += 1
+        new_price = (self.average_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1)
+        self.count_of_ratings += 1
         self.average_rating = new_price
         return new_price
 
